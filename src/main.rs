3a5a57b6ffee1d131f5d9509287af6ebc87fbb6b
Nell Shamrell-Harrington <nellshamrell@gmail.com>
2017-04-19 16:09:27 -0700

    working code and test for a list in redis
    Signed-off-by: Nell Shamrell-Harrington <nellshamrell@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,34 @@ fn main() {
     //println!("{}", hello_world());
 }
 
-pub fn get_redis_data(key: &str) -> redis::RedisResult<isize> {
-    let client = try!(redis::Client::open("redis://127.0.0.1/"));
-    let con = try!(client.get_connection());
-    con.get("my_key")
+pub fn get_redis_data(key: &str) -> redis::RedisResult<(redis::Value)> {
+    let con = try!(redis_client().get_connection());
+    let result = con.lrange("users", 0, -1);
+    result
 }
 
 pub fn create_redis_data() -> redis::RedisResult<()> {
-    let client = try!(redis::Client::open("redis://127.0.0.1/"));
-    let con = try!(client.get_connection());
+    let con = redis_client().get_connection().unwrap();
 
-    let _ :() = try!(con.set("my_key", 42));
+    con.lpush("users", "Sylvanas")?;
+    con.lpush("users", "Arthas")?;
 
     Ok(())
 }
+
+pub fn redis_cleanup() -> redis::RedisResult<()> {
+    let con = redis_client().get_connection().unwrap();
+
+    con.del("users")?;
+
+    Ok(())
+}
+
+fn redis_client() -> redis::Client {
+    let client = redis::Client::open("redis://127.0.0.1/");
+    client.unwrap()
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -27,8 +41,16 @@ mod tests {
     #[test]
     fn test_redis_connection() {
         create_redis_data();
-        let redis_data = get_redis_data("my_key");
-        assert_eq!(redis_data.unwrap(), 42);
+        let redis_data = get_redis_data("users").unwrap();
+
+        let data_results = redis::from_redis_value::<Vec<String>>(&redis_data).unwrap();
+
+
+        assert_eq!(data_results[0], "Arthas");
+        assert_eq!(data_results[1], "Sylvanas");
+
+
+        redis_cleanup();
     }
 }
 
